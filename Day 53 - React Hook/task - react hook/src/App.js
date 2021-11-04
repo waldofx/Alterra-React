df0b9a82b1157d727215f8f77346cc5a6cdae177
waldofx <90760767+waldofx@users.noreply.github.com>
2021-11-05 02:18:51 +0700

    task & exercise

---
+++ Day 53 - React Hook/task - react hook/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+// import TodoList from './components/TodoList';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
