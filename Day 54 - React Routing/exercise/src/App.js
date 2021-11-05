7f19c39a41091b44ba5cd861816ea0ac1c43a75c
waldofx <90760767+waldofx@users.noreply.github.com>
2021-11-05 16:57:34 +0700

    exercise

---
+++ Day 54 - React Routing/exercise/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./component/Home";
+import Profile from "./component/Profile";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/profile/:nama" component={Profile} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default App;
