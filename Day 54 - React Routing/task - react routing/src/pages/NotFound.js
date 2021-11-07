d306283f5ed0c658b81f9ea684b728ff2cdd9571
waldofx <90760767+waldofx@users.noreply.github.com>
2021-11-07 16:40:55 +0700

    404

--- Day 54 - React Routing/task - react routing/src/pages/NotFound.js
+++ Day 54 - React Routing/task - react routing/src/pages/NotFound.js
@@ -1,23 +1,27 @@
-import React from 'react'
-import { useHistory } from 'react-router'
+import React from "react";
+import { useHistory } from "react-router";
 
 function NotFound() {
     const history = useHistory();
 
-    function goBack(){
+    function goBack() {
         history.goBack();
     }
     return (
         <div id="notfound">
             <div class="notfound">
-                <div class="notfound-404"></div>
                 <h1>404</h1>
-                <h2>Oops! Page Not Be Found</h2>
-                <p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
-                <button onClick={goBack}>Back to homepage</button>
+                <h2>Oops! Something Went Wrong!</h2>
+                <br />
+                <p>
+                    Sorry but the page you are looking for does not exist, have
+                    been removed. name changed or is temporarily unavailable
+                </p>
+                <br />
+                <button onClick={goBack}>Return to Home</button>
             </div>
         </div>
-    )
+    );
 }
 
-export default NotFound
+export default NotFound;
