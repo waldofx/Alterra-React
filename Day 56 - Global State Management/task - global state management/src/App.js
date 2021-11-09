b0718ff16dc95b75ab68d4574ffeb9879af5a755
waldofx <90760767+waldofx@users.noreply.github.com>
2021-11-09 18:32:28 +0700

    task

---
+++ Day 56 - Global State Management/task - global state management/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About1 from "./pages/About1";
+import About2 from "./pages/About2";
+import NotFound from "./pages/NotFound";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store/store";
+
+function App() {
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <Router>
+                    <Navbar />
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/about/about-app" component={About1} />
+                        <Route path="/about/about-author" component={About2} />
+                        <Route component={NotFound} />
+                    </Switch>
+                </Router>
+            </PersistGate>
+        </Provider>
+    );
+}
+
+export default App;
