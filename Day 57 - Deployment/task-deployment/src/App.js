48e6d380675979be33b61da1a4c5f0f0228936dc
waldofx <90760767+waldofx@users.noreply.github.com>
2021-11-11 18:14:41 +0700

    deploy

---
+++ Day 57 - Deployment/task-deployment/src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Selamat Datang di Website Static Waldo Felix.</p>
+                <a
+                    className="App-link"
+                    href="https://github.com/waldofx/Alterra-React/tree/Deployment"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Github Saya
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
