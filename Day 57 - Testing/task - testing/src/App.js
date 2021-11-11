b780d247f06fcd6129e51d7bb730db233d4741c3
waldofx <90760767+waldofx@users.noreply.github.com>
2021-11-11 17:05:39 +0700

    re-added testing

---
+++ Day 57 - Testing/task - testing/src/App.js
@@ -0,0 +1,15 @@
+import FormPage from "./pages/Form/FormPage";
+import Search from "./pages/Form/Search";
+import SearchTask from "./Code/Search";
+import FormCoding from "./Code/FormCoding";
+
+function App() {
+  return (
+    // <FormCoding />
+    <SearchTask />
+    // <FormPage />
+    // <Search />
+  );
+}
+
+export default App;
